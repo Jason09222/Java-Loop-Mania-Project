5e710e57c16b1be9dfb0f0365296ed9e885edf41
ruxibro 赵奕傑 <ruxibrozhaoyijie@ruxibrodeMacBook-Pro.local>
2021-07-12 17:16:05 +0800

    defense

--- src/unsw/loopmania/Armour.java
+++ src/unsw/loopmania/Armour.java
@@ -6,8 +6,17 @@ import javafx.beans.property.SimpleIntegerProperty;
  * represents an equipped or unequipped Armour in the backend world
  */
 public class Armour extends BasicItem {
+    private final int defense = 100;
+    private final int price = 1000;
     // TODO = add more weapon/item types
     public Armour(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x, y, "Armour");
     }
+    public int getDefense() {
+        return this.defense;
+    }
+    public int getPrice() {
+        return this.price;
+    }
+    
 }
--- src/unsw/loopmania/Shield.java
+++ src/unsw/loopmania/Shield.java
@@ -7,7 +7,16 @@ import javafx.beans.property.SimpleIntegerProperty;
  */
 public class Shield extends BasicItem {
     // TODO = add more weapon/item types
+    private final int defense = 200;
+    private final int price = 2000;
     public Shield(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x, y, "Shield");
     }
+    public int getDefense() {
+        return this.defense;
+    }
+    public int getPrice() {
+        return this.price;
+    }
+    
 }
